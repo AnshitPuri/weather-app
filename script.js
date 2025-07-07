109664fe0226d5a30daca54805eadf66ede49421
Anshit <anshitpuri99@gmail.com>
2025-07-07 15:15:44 +0530

    completed UI

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const mainContainer = document.querySelector('.container')
+const displayBar = document.querySelector('.displayBar')
 const locations = document.querySelector('#location')
 const weatherIcon = document.querySelector('#weatherIcon')
 const dateTime = document.querySelector('#dateTime')
@@ -5,42 +7,69 @@ const temperature = document.querySelector('#temperature')
 const clouds = document.querySelector('#clouds')
 const humidity = document.querySelector('#humidity')
 const windSpeed = document.querySelector('#windSpeed')
+const errorDisplay = document.querySelector('#errorDisplay')
+const searchCity = document.querySelector('.searchCity')
+const mainContent = document.querySelector('.mainContent')
 
 const apiKey = '553ce1e31384467bb5c70431250707'
-let city = "Poland"
+let city = "Delhi"
 let data;
-const mainContainer = document.querySelector('.container')
 
 
 
 
 async function changeWeather() {
     try {
+
         let response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`)
         data = await response.json()
         changeToContent()
+        searchCity.style.cssText = "display: none";
+        mainContent.style.cssText = 'display: contents'
+        displayBar.replaceChildren(mainContent)
         console.log(data)
+
     } catch (error) {
-        mainContainer.textContent = "Invalid City"
+
+        searchCity.style.cssText = "display: none";
+        errorDisplay.style.cssText = "display: contents;"
+        displayBar.replaceChildren(errorDisplay);
         console.error(error)
     }
 }
 changeWeather()
 
 function changeToContent() {
-    locations.textContent = data.location.country
+
+    locations.textContent = data.location.region
+
     const iconCode = data.current.condition.icon
+
     weatherIcon.src = `https:${iconCode}`
+
     console.log(iconCode)
+
     const temperatureValue = data.current.heatindex_c
+
     temperature.textContent = `${temperatureValue} ℃`
-    // clouds.textContent = data.current.condition.text
+
+    const cloudinitial = data.current.condition.text
+
+    const cloudValue = cloudinitial.split(" ").slice(0,2).join(" ")
+    clouds.textContent = cloudValue
+
     const humidityValue = data.current.humidity
+
     humidity.textContent = `${humidityValue} %`
+
     const windSpeedValue = data.current.wind_kph
+
     windSpeed.textContent = `${windSpeedValue} K/h`
+
     const date = data.location.localtime
+
     let formattedDate = getDate(date);
+
     dateTime.textContent = formattedDate
 
 }
@@ -54,3 +83,4 @@ function getDate(dateStr) {
 
     return `${weekday}, ${day} ${month}`;
 }
+
