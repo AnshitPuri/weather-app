a186f8899af98a7729f25111cedb81bf5f874772
Anshit <anshitpuri99@gmail.com>
2025-07-07 12:49:44 +0530

    added assets

--- script.js
+++ script.js
@@ -3,14 +3,17 @@ const weatherIcon = document.querySelector('#weatherIcon')
 const dateTime = document.querySelector('#dateTime')
 const temperature = document.querySelector('#temperature')
 const clouds = document.querySelector('#clouds')
-const apiKey = 'bd5e378503939ddaee76f12ad7a97608'
-let city = "Delhi"
+const humidity = document.querySelector('#humidity')
+const windSpeed = document.querySelector('#windSpeed')
+
+const apiKey = '553ce1e31384467bb5c70431250707'
+let city = "Poland"
 let data;
 const mainContainer = document.querySelector('.container')
 
 async function changeWeather() {
     try {
-        let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
+        let response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`)
         data = await response.json()
         changeToContent()
         console.log(data)
@@ -22,8 +25,15 @@ async function changeWeather() {
 changeWeather()
 
 function changeToContent() {
-    locations.textContent = data.name
-    // dateTime.textContent = data.list[0].dt_txt
-    // let iconCode = data.list[0].weather[0].icon;
+    locations.textContent = data.location.country
+    // const iconCode = data.weather[0].icon
     // weatherIcon.src = `https://openweathermap.org/img/wn/${iconCode}@2x.png`
+    const temperatureValue = data.current.heatindex_c
+    temperature.textContent = `${temperatureValue} ℃`
+    // clouds.textContent = data.current.condition.text
+    const humidityValue = data.current.humidity
+    humidity.textContent = `${humidityValue} %`
+    const windSpeedValue = data.current.wind_kph
+    windSpeed.textContent = `${windSpeedValue} K/h`
+    
 }   
